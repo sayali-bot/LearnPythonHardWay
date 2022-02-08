8bf273eb28b34142249c1dade10d71bc8090ffcc
Sayali1792 <94345849+sayali-bot@users.noreply.github.com>
2022-02-08 10:52:58 +0530

    Added exercise 5
    String formatting operation

---
+++ ex5_more_printing.py
@@ -0,0 +1,16 @@
+#more variables and printing
+my_name = 'Sayali Salunkhe'
+my_age = 28
+my_height = 64 #inches
+my_weight = 136 # lbs
+my_eye = 'brown'
+my_teeth = 'white'
+my_hair = 'black'
+print(f"Lets talk about {my_name}.")
+print(f"She's {my_height} inches tall.")
+print(f"She's {my_weight} pounds heavy.")
+print("Actually thats not too heavy.")
+print(f"She's got {my_eye} eyes and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth} depending on coffee. ")
+total = my_age + my_height + my_weight
+print(f"If I add {my_age},{my_height}, and {my_weight} I get {total}")
