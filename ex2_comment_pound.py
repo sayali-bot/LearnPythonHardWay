03202a6dabbd1035769630bf3dde48f85e5c2fe5
Sayali1792 <94345849+sayali-bot@users.noreply.github.com>
2022-02-08 10:47:49 +0530

    Added exercise 2
    Commenting also known as pound character (#)

---
+++ ex2_comment_pound.py
@@ -0,0 +1,6 @@
+#comments and pound character
+#Anything after # is ignored by python.
+print("I could have code like this.")
+#you can also use a comment to "disable" or comment out code
+#print("This won't run")
+print("This will run.")
